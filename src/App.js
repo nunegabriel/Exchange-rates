14cc24055cf5e429c171535334370316ae9f138c
nunegabriel <nune.gabriel.gn@gmail.com>
2022-12-10 12:12:32 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-/* eslint-disable */ 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
+import Detail from './components/Coindetail';
 
 function App() {
   return (
-    <div className="App">
-      <Home />      
+    <div>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/detail/:id" element={<Detail />} />
+      </Routes>
     </div>
+
   );
-};
+}
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,54 @@
-/* eslint-disable */
-import React from "react";
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { coinDetail, fetchCoin } from '../redux/coin/home';
 
-const Home = () => {
-    return (
-        <div className="Home">
-             <div className="Hero">
-            <div className="navbar">
-                <div className="logo ">
-                    XChange
-                </div>
-            </div>
-            <div className="home-items">
-                <div className="text-desc">
-                    The Trusted Currency Authority
-                    Check exchange rates,
-                </div>
-                <div className="text-2">
-                track rates, and free currency tools
-                </div>
-                <form>
-                <input className="search" placeholder="Search Currency" />
-                <button className="btn-search">Search</button>
+const Coin = () => {
+  const coinState = useSelector((state) => state.cryptoCoin);
+  const dispatch = useDispatch();
 
-                </form>
-            </div>
+  useEffect(() => {
+    dispatch(fetchCoin());
+  }, [dispatch]);
 
+  return (
+    <div>
+      <h1 className="logo">
+        coinX
+      </h1>
+      <table>
+        <div className="crypto-coins">
+          {coinState.map((coin) => (
+
+            <tr key={uuidv4()} className={coin.baseId}>
+              <td>
+                <Link to={`/detail/${coin.baseSymbol}`}>
+                  <p>
+                    {' '}
+                    <i
+                      aria-hidden="true"
+                      className="arrow"
+                      role="button"
+                      label="next"
+                      tabIndex={0}
+                      onClick={() => dispatch(coinDetail(coin.baseSymbol))}
+                    />
+                  </p>
+                </Link>
+              </td>
+              <td>
+                {coin.baseSymbol}
+              </td>
+              <td className="value">
+                {coin.priceUsd}
+              </td>
+            </tr>
+          ))}
         </div>
-        </div>
-    )
-}
+      </table>
+    </div>
+  );
+};
 
-export default Home;
+export default Coin;
