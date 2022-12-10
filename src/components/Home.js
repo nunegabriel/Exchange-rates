a452f0564c7acb29384de0fe82e20dfc6b226f55
nunegabriel <nune.gabriel.gn@gmail.com>
2022-12-10 20:26:51 +0000

    Add Filter

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
@@ -7,6 +7,8 @@ import { coinDetail, fetchCoin } from '../redux/coin/home';
 const Coin = () => {
   const coinState = useSelector((state) => state.cryptoCoin);
   const dispatch = useDispatch();
+
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     dispatch(fetchCoin());
@@ -17,34 +19,42 @@ const Coin = () => {
       <h1 className="logo">
         coinX
       </h1>
+      <form className="input-bar" onChange={(e) => setSearch(e.target.value)}>
+        <input placeholder="PLEASE ENTER COIN SYMBOL" />
+      </form>
       <table>
         <div className="crypto-coins">
-          {coinState.map((coin) => (
+          {/* {coinState.map((coin) => ( */}
+          {coinState
+            .filter((coin) => (search.toLowerCase() === ''
+              ? coin
+              : coin.baseSymbol.toLowerCase().includes(search)))
+            .map((coin) => (
 
-            <tr key={uuidv4()} className={coin.baseId}>
-              <td>
-                <Link to={`/detail/${coin.baseSymbol}`}>
-                  <p>
-                    {' '}
-                    <i
-                      aria-hidden="true"
-                      className="arrow"
-                      role="button"
-                      label="next"
-                      tabIndex={0}
-                      onClick={() => dispatch(coinDetail(coin.baseSymbol))}
-                    />
-                  </p>
-                </Link>
-              </td>
-              <td>
-                {coin.baseSymbol}
-              </td>
-              <td className="value">
-                {coin.priceUsd}
-              </td>
-            </tr>
-          ))}
+              <tr key={uuidv4()} className={coin.baseId}>
+                <td>
+                  <Link to={`/detail/${coin.baseSymbol}`}>
+                    <p>
+                      {' '}
+                      <i
+                        aria-hidden="true"
+                        className="arrow"
+                        role="button"
+                        label="next"
+                        tabIndex={0}
+                        onClick={() => dispatch(coinDetail(coin.baseSymbol))}
+                      />
+                    </p>
+                  </Link>
+                </td>
+                <td>
+                  {coin.baseSymbol}
+                </td>
+                <td className="value">
+                  {coin.priceUsd}
+                </td>
+              </tr>
+            ))}
         </div>
       </table>
     </div>
